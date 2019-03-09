9cd1a911326947d8daedb658fce814522def96f5
Alex Wong <alw1746@users.noreply.github.com>
2019-03-09 21:01:51 +1100

    Add files via upload

--- examples/RandomNumbers/RandomNumbers.ino
+++ examples/RandomNumbers/RandomNumbers.ino
@@ -1,1 +1,395 @@
+/*
+  Display a 4-digit randomly generated number with some special effects such as blank,brightness,etc.
+  If JP1 is open, the word PIC-CLUB is shown instead. Looks up an ASCII table defined in flash memory
+  for the bit=to-segment map of a character. Not every character can be displayed on a 7 segment LED.
+  
+   bit positions(0-15) of a displayBuffer element corresponds to segments of an led.
+      0
+   +----+
+ 5 |  6 | 1
+   +----+
+12 |    | 2
+   +----+   o 11
+     13
 
+Eg:
+1 << 0 = top seg
+1 << 1 = top right seg
+1 << 5 = top left seg
+1 << 11 = decimal dot
+1 << 13 = bot seg
+
+displayBuffer elements [ 1 | 5 | 7 | 0 ] corresponds to 4 physical leds (left->right).
+displayBuffer elements [ 4 ] - degree dot
+displayBuffer elements [ 6 ] - colon
+Eg:
+displayBuffer[0] = 1<<0 | 1<<5 | 1<<11 | 1<<12 | 1<<13 | 1<<6 | 1<<1 | 1<<2;    //digit 8. in segments, rightmost LED
+displayBuffer[1] = 0b0011100001100111;   //digit 8. in binary, leftmost LED
+displayBuffer[4] = 1 << 3;  (degree dot)
+displayBuffer[6] = 1 << 4;  (colon)
+
+ */
+
+#include <Wire.h>
+
+//Blink rate
+#define  __HT16K33_BLINKRATE_OFF     0x00
+#define  __HT16K33_BLINKRATE_2HZ     0x01
+#define  __HT16K33_BLINKRATE_1HZ     0x02
+#define  __HT16K33_BLINKRATE_HALFHZ  0x03
+#define  __HT16K33_ADDRESS_KEY_DATA  0x40
+
+void setColon(bool state=true);
+void setDegrees(bool state=true);
+void setDP(uint8_t digit,bool state=true);
+void writeDigitNum(uint8_t d, uint8_t num, bool dot=false);
+
+const uint8_t addr = 0x70; // HT16K33 default address
+const int potPin  = A0;
+uint8_t ledarray[]={1,5,7,0};
+uint8_t randnum;
+uint16_t displayBuffer[8];
+
+static const uint16_t alphafonttable[] PROGMEM =  {
+0b0000000000000001,
+0b0000000000000010,
+0b0000000000000100,
+0b0000000000001000,
+0b0000000000010000,
+0b0000000000100000,
+0b0000000001000000,
+0b0000000010000000,
+0b0000000100000000,
+0b0000001000000000,
+0b0000010000000000,
+0b0000100000000000,
+0b0001000000000000,
+0b0010000000000000,
+0b0100000000000000,
+0b1000000000000000,
+0b0000000000000000,
+0b0000000000000000,
+0b0000000000000000,
+0b0000000000000000,
+0b0000000000000000,
+0b0000000000000000,
+0b0000000000000000,
+0b0000000000000000,
+0b0001001011001001,
+0b0001010111000000,
+0b0001001011111001,
+0b0000000011100011,
+0b0000010100110000,
+0b0001001011001000,
+0b0011101000000000,
+0b0001011100000000,
+0b0000000000000000, //  
+0b0000000000000110, // !
+0b0000001000100000, // "
+0b0001001011001110, // #
+0b0001001011101101, // $
+0b0000110000100100, // %
+0b0010001101011101, // &
+0b0000000000001000, // '
+0b0010010000000000, // (
+0b0000100100000000, // )
+0b0011111111000000, // *
+0b0001001011000000, // +
+0b0000100000000000, // ,
+0b0000000001000000, // -
+0b0000100000000000, // .
+0b0000110000000000, // /
+0b0011000000100111, // 0
+0b0000000000000110, // 1
+0b0011000001000011, // 2
+0b0010000001000111, // 3
+0b0000000001100110, // 4
+0b0010000001100101, // 5
+0b0011000001100101, // 6
+0b0000000000000111, // 7
+0b0011000001100111, // 8
+0b0000000001100111, // 9
+0b0000000000010000, // :
+0b0000101000000000, // ;
+0b0010010000000000, // <
+0b0010000001000000, // =
+0b0000100100000000, // >
+0b0001000010000011, // ?
+0b0000001010111011, // @
+0b0001000001100111, // A
+0b0011000001100100, // B
+0b0011000000100001, // C
+0b0011000001000110, // D
+0b0011000001100001, // E
+0b0001000001100001, // F
+0b0010000001100111, // G
+0b0001000001100110, // H
+0b0000000000000110, // I
+0b0011000000000110, // J
+0b0010010001110000, // K
+0b0011000000100000, // L
+0b0000010100110110, // M
+0b0001000001000100, // N
+0b0011000000100111, // O
+0b0001000001100011, // P
+0b0010000000111111, // Q
+0b0001000001000000, // R
+0b0010000001100101, // S
+0b0001000001100000, // T
+0b0011000000100110, // U
+0b0000110000110000, // V
+0b0010100000110110, // W
+0b0010110100000000, // X
+0b0010000001100110, // Y
+0b0000110000001001, // Z
+0b0011000000100001, // [
+0b0000000000000000, // 
+0b0010000000000111, // ]
+0b0000110000000011, // ^
+0b0010000000000000, // _
+0b0000000100000000, // `
+0b0001000001100111, // a
+0b0011000001100100, // b
+0b0011000001000000, // c
+0b0011000001000110, // d
+0b0011000001100001, // e
+0b0001000001100001, // f
+0b0010000001100111, // g
+0b0001000001100100, // h
+0b0001000000000000, // i
+0b0011000000000110, // j
+0b0011011000000000, // k
+0b0001000000100000, // l
+0b0001000011010100, // m
+0b0001000001000100, // n
+0b0011000001000100, // o
+0b0001000001100011, // p
+0b0000010010000110, // q
+0b0001000001000000, // r
+0b0010000001100101, // s
+0b0001000001100000, // t
+0b0011000000000100, // u
+0b0010000000000100, // v
+0b0010100000010100, // w
+0b0010100011000000, // x
+0b0010000001100110, // y
+0b0000100001001000, // z
+0b0000100101001001, // {
+0b0000000000000110, // |
+0b0010010010001001, // }
+0b0000010100100000, // ~
+0b0011111111111111
+};
+
+
+void setup() {
+  Serial.begin(115200);
+  Wire.begin();
+  Wire.beginTransmission(addr);
+  Wire.write(0x20 | 1); // turn on oscillator
+  Wire.endTransmission();
+  setBrightness(15);
+  blink(0);
+  randomSeed(analogRead(potPin));
+}
+
+void loop() {
+
+  uint16_t keybuf=getJumpers();
+  if (JP1_closed(keybuf))
+  {
+    for (int i=0; i < 4; i++) {
+      randnum=(int)random(0,9999)/1000;
+      //Serial.println(randnum);
+      writeDigitFx(ledarray[i],randnum);
+    }
+    blink(__HT16K33_BLINKRATE_2HZ);
+    for (int i=15; i > -1; i--) {
+      setBrightness(i);
+      delay(250);
+    }
+    blink(__HT16K33_BLINKRATE_OFF);
+    blank();
+    delay(1000);
+    blank();
+    for (int i=0; i < 16; i++) {
+      setBrightness(i);
+      delay(250);
+    }
+    clearFx();
+    delay(500);
+  }
+  else
+  {
+    writeDigitAscii(1,'P');
+    writeDigitAscii(5,'I');
+    writeDigitAscii(7,'C');
+    writeDigitAscii(0,'-');
+    show();
+    delay(1000);
+    clear();
+    writeDigitAscii(1,'C');
+    writeDigitAscii(5,'L');
+    writeDigitAscii(7,'U');
+    writeDigitAscii(0,'B');
+    show();
+    delay(1000);
+    clear();
+  }
+}
+
+void show(){
+  Wire.beginTransmission(addr);
+  Wire.write(0x00); // start at address 0x0
+
+  for (int i = 0; i < 8; i++) {
+    Wire.write(displayBuffer[i] & 0xFF);    
+    Wire.write(displayBuffer[i] >> 8);    
+  }
+  Wire.endTransmission();  
+}
+
+void clear(){
+  for(int i = 0; i < 8; i++){
+    displayBuffer[i] = 0;
+  }
+}
+
+void clearFx(){
+  byte bufmap[]={1,5,7,0};
+  for(int i = 3; i > -1; i--){
+    displayBuffer[bufmap[i]] = 0;
+    show();
+    segmentOn(bufmap[i],0);
+    segmentOn(bufmap[i],5);
+    segmentOn(bufmap[i],12);
+    segmentOn(bufmap[i],13);
+    segmentOn(bufmap[i],2);
+    segmentOn(bufmap[i],1);
+    displayBuffer[bufmap[i]] = 0;
+    show();
+  }
+}
+
+void setBrightness(uint8_t b){
+  if(b > 15) return;
+
+  Wire.beginTransmission(addr);
+  Wire.write(0xE0 | b); // Dimming command
+  Wire.endTransmission();
+}
+
+void blank(){
+  static boolean blankOn;  
+
+  Wire.beginTransmission(addr);
+  Wire.write(0x80 | blankOn); // Blanking / blinking command
+  Wire.endTransmission();
+
+  blankOn = !blankOn;
+}
+
+void blink(uint8_t b){
+  if(b > 3) return;
+
+  Wire.beginTransmission(addr);
+  Wire.write(0x80 | b << 1 | 1); // Blinking / blanking command
+  Wire.endTransmission();
+}
+
+void writeDigitAscii(uint8_t n, uint8_t a) {
+  if (n > 7) return;
+  uint16_t font = pgm_read_word(alphafonttable+a);
+  displayBuffer[n] = font;
+}
+
+void writeDigitRaw(uint8_t d, uint16_t bitmask) {
+  if (d > 7) return;
+  displayBuffer[d] = bitmask;
+}
+
+void writeDigitNum(uint8_t d, uint8_t num, boolean dot) {
+  uint16_t dotfont=0;
+  
+  if (d > 7) return;
+  uint16_t numfont = pgm_read_word(alphafonttable+'0'+num);
+  if (dot)
+   dotfont=pgm_read_word(alphafonttable+'.');
+  writeDigitRaw(d, numfont | dotfont);
+}
+
+void setColon(bool state) {
+  if (state)
+    displayBuffer[6] |= 1 << 4;
+  else
+    displayBuffer[6] &= ~(1 << 4);
+}
+
+void setDegrees(bool state) {
+  if (state)
+    displayBuffer[4] |= 1 << 3;
+  else
+    displayBuffer[4] &= ~(1 << 3);
+}
+
+void setDP(uint8_t digit,bool state) {
+  uint16_t pattern;
+  if (digit > 7)
+    return;
+  if (state)
+    displayBuffer[digit] |= 1 << 11;
+  else
+    displayBuffer[digit] &= ~(1 << 11);
+}
+
+uint16_t getKeys(uint8_t row) {
+  uint8_t byteval[2];
+  uint8_t numbytes;
+
+  numbytes=2;
+  Wire.beginTransmission(addr);
+  Wire.write(__HT16K33_ADDRESS_KEY_DATA + row*2); 
+  Wire.endTransmission(); 
+  Wire.requestFrom(addr,numbytes);
+  int count=Wire.available();
+  if (count == 2) {
+    byteval[0] = Wire.read();
+    byteval[1] = Wire.read();    //1st 3 bits = JP3,2,1 states
+  }
+  return byteval[1];
+}
+
+uint16_t getJumpers() {
+    getKeys(0);
+    delay(25);
+    return getKeys(0);
+}
+
+uint8_t JP1_closed(uint16_t keybuf) {
+  return (keybuf & 0b001);
+}
+
+uint8_t JP2_closed(uint16_t keybuf) {
+  return (keybuf & 0b010);
+}
+
+uint8_t JP3_closed(uint16_t keybuf) {
+  return (keybuf & 0b100);
+}
+
+void segmentOn(byte i,uint16_t bitpos) {
+  displayBuffer[i] |= 1 << bitpos;
+  show();
+  delay(80);
+}
+
+void writeDigitFx(byte i,uint8_t chr) {
+  segmentOn(i,0);
+  segmentOn(i,1);
+  segmentOn(i,2);
+  segmentOn(i,13);
+  segmentOn(i,12);
+  segmentOn(i,5);
+  writeDigitNum(i,chr);
+  show();
+}
